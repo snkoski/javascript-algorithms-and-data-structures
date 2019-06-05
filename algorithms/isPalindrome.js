45da040d16bbe7e97eeaba36a60e6e535d0ce290
snkoski <shawnnkoski@gmail.com>
2019-06-05 15:54:01 -0400

    First commit

---
+++ algorithms/isPalindrome.js
@@ -0,0 +1,9 @@
+function isPalindrome(str) {
+    if (str.length === 0 || str.length === 1) return true;
+    if (str[0] === str[str.length - 1]) {
+        return isPalindrome(str.slice(1, str.length - 1));
+        }
+    return false;
+}
+
+isPalindrome('racecar')
